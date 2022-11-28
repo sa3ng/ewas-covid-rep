b00ff65cacdbd4f35fbcbf05b60676d8ae7e77e9
Edgar <epicerds2@gmail.com>
2022-11-28 21:32:23 +0800

    activity_edit_profile.xml and EditProfileActivity.java setup for changing user credentials

--- app/src/main/java/com/mp3/ewas_covid_app/Activities/EditProfileActivity.java
+++ app/src/main/java/com/mp3/ewas_covid_app/Activities/EditProfileActivity.java
@@ -1,9 +1,11 @@
 package com.mp3.ewas_covid_app.Activities;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.google.android.material.textfield.TextInputLayout;
 import com.mp3.ewas_covid_app.R;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -12,5 +14,17 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        TextInputLayout tilEmail = findViewById(R.id.activity_edit_profile__til_email);
+        TextInputLayout tilName = findViewById(R.id.activity_edit_profile__til_name);
+        TextInputLayout tilNumber = findViewById(R.id.activity_edit_profile__til_number);
+        TextInputLayout tilGender = findViewById(R.id.activity_edit_profile__til_gender);
+
+        Button btnEdit = findViewById(R.id.activity_edit_profile__btn_edit);
+        btnEdit.setOnClickListener(view -> {
+
+            /* return to previous activity (which should be activity_edit_profile) */
+            finishActivity(0);
+        });
     }
 }
